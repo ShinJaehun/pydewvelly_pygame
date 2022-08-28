10496140ccc8775a9aad9381e14d52bcfedf0e4e
ShinJaehun <gunsmoke.shin@gmail.com>
2022-08-28 14:29:03 +0900

    switch between tools

--- code/player.py
+++ code/player.py
@@ -22,14 +22,28 @@ class Player(pygame.sprite.Sprite):
 
         # timers for tool use
         self.timers = {
-            'tool use' : Timer(350, self.use_tool)
+            'tool use' : Timer(350, self.use_tool),
+            'tool switch' : Timer(200),
+            'seed use' : Timer(350, self.use_seed),
+            'seed switch' : Timer(200)
         }
 
         # tools
-        self.selected_tool = 'axe'
+        self.tools = ['hoe', 'axe', 'water']
+        self.tool_index = 0
+        self.selected_tool = self.tools[self.tool_index]
+
+        # seeds
+        self.seeds = ['corn', 'tomato']
+        self.seed_index = 0
+        self.selected_seed = self.seeds[self.seed_index]
 
     def use_tool(self):
-        print(self.selected_tool)
+        # print(self.selected_tool)
+        pass
+
+    def use_seed(self):
+        pass
 
     def import_assets(self):
         self.animations = {'up': [],'down': [],'left': [],'right': [],
@@ -76,6 +90,28 @@ class Player(pygame.sprite.Sprite):
                 self.timers['tool use'].activate()
                 self.direction = pygame.math.Vector2() #이게 꼭 필요한지는 모르겠음.
                 self.frame_index = 0
+
+            # change tools
+            if keys[pygame.K_q] and not self.timers['tool switch'].active:
+                self.timers['tool switch'].activate()
+                self.tool_index += 1
+                self.tool_index = self.tool_index if self.tool_index < len(self.tools) else 0
+                self.selected_tool = self.tools[self.tool_index]
+
+            # seed use
+            if keys[pygame.K_LCTRL]:
+                self.timers['seed use'].activate()
+                self.direction = pygame.math.Vector2()
+                self.frame_index = 0
+                # print('use seed')
+
+            # change seeds
+            if keys[pygame.K_e] and not self.timers['seed switch'].active:
+                self.timers['seed switch'].activate()
+                self.seed_index += 1
+                self.seed_index = self.seed_index if self.seed_index < len(self.seeds) else 0
+                self.selected_seed = self.seeds[self.seed_index]
+                # print(self.selected_seed)
 
     def get_status(self):
         # add _idle to the status if the player is not moving
